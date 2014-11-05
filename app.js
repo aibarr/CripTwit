4681f2dbac15be9f312caf112eb7ab39772c1357
aibarr <alfredo.barra@usach.cl>
2014-11-05 00:09:52 -0300

    Se agrega jQuery de manera local. Se agregan paneles a vista de recepción. Se modifica app.js para aceptar al emisor.

--- app.js
+++ app.js
@@ -40,11 +40,78 @@ app.listen(3000, function(){
 
 //Configurando socket
 
+var participantes = 0;
+var emisor = {
+  soquete : null,
+  hay : 0};
+var receptor = {
+  soquete : null,
+  hay : 0};
+
 io.sockets.on('connect', function(socket){
   console.log('Alguien se ha conectado');
 
+  socket.rol = 'nada';
+
+  //cuando llegue un emisor
+  socket.on('enviar', function(data, callback){
+    //Digo que sí hay un emisor presente
+    console.log('Tenemos al emisor');
+    emisor.soquete = socket;
+    emisor.hay = 1;
+    socket.rol = 'emisor';
+    //Si hay un receptor
+    if(receptor.hay){
+      //aviso que sí hay quien reciba
+      socket.receptor = receptor.soquete;
+      callback(true);
+      //Y despierto al receptor
+    }else{
+      //Sino, aviso que no hay quien reciba
+      callback(false);
+    }
+  });
+
+  socket.on('recibir', function(data, callback){
+    //digo que sí hay un receptor presente
+    receptor.soquete = socket;
+    receptor.hay = 1;
+    socket.rol = 'receptor';
+    //Si hay un emisor
+    if(emisor.hay){
+      //aviso que sí hay quien envíe
+      socket.emisor = emisor.soquete;
+      callback(true);
+      //Y despierto al emisor
+    }else{
+      //Sino, aviso que no hay quien reciba
+      callback(false);
+    }
+  });
+
+  //Cuando llegue un mensaje del emisor
+  socket.on('mensaje', function(data, callback){
+    //Le envío el mensaje al receptor
+    socket.receptor.emit('mensaje', data);
+  });
+
   //Registro cuando un usuario se salio del servidor
   socket.on('disconnect', function(){
+    switch(socket.rol){
+      case 'emisor':
+        emisor = false;
+        break;
+      case 'receptor':
+        receptor = false;
+        break;
+      case 'nada':
+        //doNothing
+        break;
+      default:
+        //doNothing
+        break;
+    }
+
     console.log('Alguien se ha desconectado');
   });
 
