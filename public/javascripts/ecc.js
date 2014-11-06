efaf260e2d7524fe0b58f1f14aa0f6dddd658249
aibarr <alfredo.barra@usach.cl>
2014-11-06 13:36:41 -0300

    Agregada encriptación DES entre emisor y receptor. Falta ingresar claves y vector inicial.

---
+++ public/javascripts/ecc.js
@@ -0,0 +1,19 @@
+//ECC: Encriptado curva elíptica
+//Y²=x³+ax+b
+
+function encECC(tPlano){
+	//Cambiar el textoplano a entero
+	
+	var tCifrado;
+	return tCifrado;
+}
+
+function sumaEliptica(a, b){
+	var resultado;
+	return resultado;
+}
+
+function multiEpliptica(veces, multiplicando){
+	var resultado;
+	return resultado;
+}
