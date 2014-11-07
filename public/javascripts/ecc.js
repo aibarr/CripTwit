01dec450fd3ac157ad6a0cde9e6e12649079b07c
moyarzun <mauricio2141@gmail.com>
2014-11-07 17:51:17 -0300

    Arreglando el ultimo paso de tablas

--- public/javascripts/ecc.js
+++ public/javascripts/ecc.js
@@ -33,9 +33,57 @@ function esPrimo(numero){
 
 // Validación: comprueba si (a,b) es válido
 function sonCtesValidas(a, b, p){
-	if ((4*a^3+27*b^2)%p != 0){
+	if ((4*Math.pow(a,3)+27*Math.pow(b,2))%p != 0){
 		return true;
 	}else {
 		return false;
 	}
+}
+
+// Construcción: crea tablas de puntos para constantes ECC dadas
+function puntosECC(a, b, p, ga, gb){
+	var paso1 = [];
+	var paso2 = [];
+	var paso3 = [];
+	var paso4 = [];
+	var paso5 = [];
+
+	for(i = 0; i < p; i++){
+		paso3[i] = [];
+		paso4[i] = [];
+	}
+
+	for(i = 0; i < p; i++){
+		paso1[i] = (Math.pow(i,3)+a*i+b)%p;
+	}
+	//return paso1;
+
+	for(i = 0; i < p; i++){
+		paso2[i] = Math.pow(i,2)%p;
+	}
+	//return paso2;
+
+	for(i = 0; i < p; i++){
+		paso3[paso2[i]].push(i);
+	}
+	//return paso3;
+
+	for(i = 0; i < p; i++){
+		paso4[i] = paso3[paso1[i]];
+	}
+	//return paso4[0].length;
+
+	var contador = 0;
+	for(i = 0; i < p; i++){
+		if(paso4[i].length > 0){
+			var aux = [];
+			for(j = 0; j < paso4[i].length; j++){
+				paso5[contador] = []
+				paso5[contador].push(i);
+				paso5[contador].push(paso4[i][j]);
+				contador++;
+			}
+		}
+	}
+	return paso5;
 }
