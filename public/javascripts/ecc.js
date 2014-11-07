c7f52304c421acaf94ddda8127be31ccd01eb87d
moyarzun <mauricio2141@gmail.com>
2014-11-07 16:34:56 -0300

    Validaciones para ecc
    Valida si el numero es primo, si los puntos escogidos son validos.

--- public/javascripts/ecc.js
+++ public/javascripts/ecc.js
@@ -3,7 +3,6 @@
 
 function encECC(tPlano){
 	//Cambiar el textoplano a entero
-	
 	var tCifrado;
 	return tCifrado;
 }
@@ -16,4 +15,27 @@ function sumaEliptica(a, b){
 function multiEpliptica(veces, multiplicando){
 	var resultado;
 	return resultado;
+}
+
+// Validación: comprueba si 'numero' es primo.
+function esPrimo(numero){
+	if(numero == 1 || numero == 2){
+		return true;
+	} else{
+		for (i = 2; i < numero; i++){
+			if(numero % i == 0){
+				return false;
+			}
+		return true;
+		}
+	}
+}
+
+// Validación: comprueba si (a,b) es válido
+function sonCtesValidas(a, b, p){
+	if ((4*a^3+27*b^2)%p != 0){
+		return true;
+	}else {
+		return false;
+	}
 }
